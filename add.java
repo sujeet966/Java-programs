5f0253a8fe96e5351b4cf08becf6f2dcad2e83c6
sujeet966 <43859512+sujeet966@users.noreply.github.com>
2020-01-08 11:06:39 +0530

    we are adding two numbers
    simply enter two numbers and get sum of the given two numbers

---
+++ add.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class test
+{
+  public static void main(String arg[])
+  {
+      Scanner sc = new Scanner(syystem.in);
+      int x = sc.nextInt();
+      int y = sc.nextInt();
+      System.out.println(x+y);
+  }
+}
